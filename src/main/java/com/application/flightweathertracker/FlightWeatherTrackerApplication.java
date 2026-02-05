6d45dda8ec6483fd109ee25f70e4f2eb7c3f4e42
kotick0 <krystowskipiotr2005@gmail.com>
2026-02-05 15:39:19 +0100

    first commit

---
+++ src/main/java/com/application/flightweathertracker/FlightWeatherTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.application.flightweathertracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightWeatherTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlightWeatherTrackerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/application/flightweathertracker/FlightWeatherTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.application.flightweathertracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightWeatherTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
